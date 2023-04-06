55a45c7a8b34933d43decb7af514527813317828
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-04-06 13:06:22 -0400

    added state to body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Body() {
+	const [search, setSearch] = useState("state");
+	const [history, setHistory] = useState([]);
+
+	const searchCity = (event) => {
+		event.preventDefault();
+		setHistory([...history, search]);
+	};
+
 	return (
 		<div className="">
 			<form>
 				<label>Search for a City</label>
-				<input id="search" type="text" />
+				<input
+					id="search"
+					type="text"
+					onChange={(event) => setSearch(event.target.value)}
+				/>
+				<button onClick={searchCity}>Search</button>
 			</form>
+			<div>
+				<ul>
+					{history.map((city, i) => {
+						return <li key={i}>{city}</li>;
+					})}
+				</ul>
+			</div>
 
 			<div>current weather display</div>
 			<div>forecast</div>
